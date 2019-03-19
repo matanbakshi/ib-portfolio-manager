bbc59e3d8e68fdfb739f60b533acb6188b960bd6
Matan Bakshi <matan.bakshi@gmail.com>
2019-03-19 14:14:52 +0200

    Login is now fully automated from running the gateway to authentication

--- src/utils/ib_gateway_launcher.py
+++ src/utils/ib_gateway_launcher.py
@@ -1,24 +1,49 @@
-import subprocess
+import subprocess, fcntl, os
+from time import sleep
+
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import json
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 from src.logger import logger
 
 LOGIN_SUCCEEDS_PHRASE = "Client login succeeds"
-BIN_HEADLESS_CHROMIUM = "../bin/headless-chromium"
 CREDS_FILE_PATH = "../config/ib_creds.json"
+GATEWAY_RUNNING_PHRASE = "Open https://localhost:5000 to login"
+GATEWAY_RUN_PATH = "bin/run.sh"
+GATEWAY_CONF_ATH = "root/conf.yaml"
+GATEWAY_WORKDIR_PATH = "/home/matan/clientportal.beta.gw"
 
 
-def launch_ib_gateway(run_script_path, conf_path, work_dir_path):
-    subprocess.Popen([run_script_path, conf_path], cwd=work_dir_path)
+def launch_ib_gateway():
+    _open_gateway_process()
+    _automate_auth()
 
-    # Authenticate via headless browser
 
-    pass
+def _open_gateway_process():
+    p = subprocess.Popen([GATEWAY_RUN_PATH, GATEWAY_CONF_ATH], cwd=GATEWAY_WORKDIR_PATH,
+                         stdout=subprocess.PIPE)
+
+    # Reading stdout without blocking (https://stackoverflow.com/a/8980466/10249811)
+    fcntl.fcntl(p.stdout.fileno(), fcntl.F_SETFL, os.O_NONBLOCK)
+
+    # Read stdout until gateway launch finish
+    sleep(1)
+    aggregated_output = b""
+    current_output = p.stdout.read()
+    while current_output:
+        aggregated_output += current_output
+        sleep(0.5)
+        current_output = p.stdout.read()
+
+    if GATEWAY_RUNNING_PHRASE not in str(aggregated_output):
+        raise SystemError(f"IB gateway failed to launch, output: {aggregated_output}")
+
+    return True
 
 
 def _load_ib_creds():
@@ -31,14 +56,7 @@ def _automate_auth():
     user_name, password = _load_ib_creds()
 
     driver = webdriver.PhantomJS(service_args=['--ignore-ssl-errors=true'])
-
     driver.get("https://localhost:5000")
-
-    # try:
-    #     WebDriverWait(driver, 3).until(EC.presence_of_all_elements_located((By.ID, "user_name")))
-    # except TimeoutException:
-    #     logger.error("Login to IB failed, page loading timed out")
-    #     raise
 
     un_box = driver.find_element_by_id("user_name")
     pw_box = driver.find_element_by_id("password")
@@ -46,14 +64,19 @@ def _automate_auth():
 
     un_box.send_keys(user_name)
     pw_box.send_keys(password)
-
     submit_btn.click()
 
     try:
-        assert LOGIN_SUCCEEDS_PHRASE in driver.page_source
+        WebDriverWait(driver, 3).until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, "pre"), LOGIN_SUCCEEDS_PHRASE))
+    except TimeoutException:
+        logger.error("Login to IB failed, page loading timed out")
+        raise
     finally:
         driver.quit()
 
+    return True
+
 
 if __name__ == "__main__":
-    _automate_auth()
+    launch_ib_gateway()
